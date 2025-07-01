d2d95aa897e96f0f5298d3e1c69085a5a919942c
aivisualny <aivisualny@gmail.com>
2025-07-02 01:39:51 +0900

    solve the button error

--- frontend/app/mypage/page.tsx
+++ frontend/app/mypage/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Button from "@/components/ui/Button";
+import { Button } from "@/components/ui/Button";
 import { getMe, getMyVotesFiltered, getMyVoteStats, getMyBadges, getFollowers, getFollowing, getMyRequestedReviews, withdraw } from "@/lib/api";
 import { User, Vote, VoteStats, UserBadge } from "@/types/content";
 
